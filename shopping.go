108320a5a131399f27eb522f1c8e37a24c55bed3
thalestmm <meier.thales@gmail.com>
2025-02-17 13:13:09 -0300

    removed compare function, the logic is inside the routes.go fn

--- shopping.go
+++ shopping.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
-
 type ShoppingRequest struct {
 	BaseValue      float64 `json:"base_value" xml:"base_value" form:"base_value"`
 	DurationMonths int     `json:"duration_months" xml:"duration_months" form:"duration_months"`
@@ -27,20 +22,5 @@ func simulateInvestmentWithPayments(baseValue float64, durationMonths int, rate 
 		return taxedTotal, nil
 	} else {
 		return total, nil
-	}
-}
-
-func compareDiscountToInvestment(baseValue float64, discountPct float64,
-	durationMonths int, monthlyRate float64, applyTax bool) {
-	investmentTotal, err := simulateInvestmentWithPayments(baseValue, durationMonths, monthlyRate, applyTax)
-	if err != nil {
-		log.Fatal(err)
-	}
-	discountedValue := (1 - discountPct/100.0) * baseValue
-	diff := baseValue - discountedValue
-	if investmentTotal > diff {
-		fmt.Printf("It's better to invest by $%.2f \n", investmentTotal-diff)
-	} else {
-		fmt.Printf("Buy it with the discount, you'll save %.2f \n", diff-investmentTotal)
 	}
 }
